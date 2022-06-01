6567f2040cdf89440e7011573217a3e44622a5a8
mjagintowicz <102296275+mjagintowicz@users.noreply.github.com>
2022-06-01 16:51:27 +0200

    Update Card.cpp

--- Card/Card.cpp
+++ Card/Card.cpp
@@ -1,2 +1,7 @@
 #include "Card.h"
 
+
+Class::Class(const int value, const in suit) : value_(value), suit_(suit) {
+	assert(suit_ == HEARTS || suit_ == CLUBS || suit_ == DIAMONDS || suit_ == SPADES);
+	assert(value_ >= LOWEST_CARD_VALUE && value_ <= HIGHEST_CARD_VALUE);
+}
