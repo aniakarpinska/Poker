19dce33c2ee19c5c13c4490f6b2464965cf79b24
KaczMac <100795809+KaczMac@users.noreply.github.com>
2022-06-01 22:15:17 +0200

    Create CommonFunctions.h

---
+++ Game_core/CommonFunctions.h
@@ -0,0 +1,19 @@
+#ifndef COMMONFUNCTIONS_H_
+#define COMMONFUNCTIONS_H_
+
+#include <sstream>
+
+using std::stringstream;
+
+namespace PokerUtils {
+
+	template <class T>
+	std::string AutoToString(const T& obj) {
+		stringstream ss;
+		ss << obj;
+		return ss.str();
+	}
+
+}
+
+#endif
