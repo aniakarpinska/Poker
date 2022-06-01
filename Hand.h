e953bde31014cbc9fa9cfa162d9644d6e75b14a3
KaczMac <100795809+KaczMac@users.noreply.github.com>
2022-06-01 21:52:43 +0200

    Create Hand.h

---
+++ Hand.h
@@ -0,0 +1,32 @@
+#ifndef HAND_H_
+#define HAND_H_
+
+#include "Card.h"
+#include <string>
+#include <vector>
+
+enum HandType {
+	HT_NO_HAND,
+	HT_HIGH_CARD,
+	HT_PAIR,
+	HT_TWO_PAIR,
+	HT_STRAIGHT,
+	HT_THREE_OF_A_KIND,
+	HT_FLUSH,
+	HT_FULL_HOUSE,
+	HT_FOUR_OF_A_KIND,
+	HT_STRAIGHT_FLUSH
+};
+const unsigned int MAX_CARDS_IN_HAND = 7;
+
+typedef struct _HandValue {
+	HandType type;
+	const Card *primaryCard;
+	const Card *secondaryCard;
+} HandValue;
+
+class Hand{
+public:
+  size_t GetCardCount() const { return m_hand.size(); }
+  int hand_compare(const Hand* hand1, const Hand* hand2);
+}
